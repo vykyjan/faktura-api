78cb942bdb29ed51e22ce0d9fa36f6158d16a7a1
Jan Vykydal <vykyjan@gmail.com>
2013-10-10 20:35:47 +0200

    new api

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FakturaApi::Application.config.secret_key_base = 'f69c4a8f50e8576fcd0f7fcd212aaf08e93279054d4d05ebeeb834fb05cd077248684e652ff02450a3b38db33cd80e1b72e3fd42c8c43e3f6a22a9bdf1b049c9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FakturaApi::Application.config.session_store :cookie_store, key: '_faktura-api_session'
