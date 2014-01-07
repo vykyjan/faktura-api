db847ebdf92f6051a50ac370ef4927f0f6f443e8
Jan Vykydal <vykyjan@gmail.com>
2014-01-07 12:48:37 +0100

    sendgrid setting config action2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ FakturaApi::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.action_mailer.default_url_options = { :host => 'your.websitedomain.com' }
+  config.action_mailer.default_url_options = { :host => 'http://faktura-api.herokuapp.com/' }
   ActionMailer::Base.smtp_settings = {
       :address        => 'smtp.sendgrid.net',
       :port           => '587',
