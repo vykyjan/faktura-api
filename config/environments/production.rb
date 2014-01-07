abf4fd17881a02299291a989f2e41c3d0ede5ec4
Jan Vykydal <vykyjan@gmail.com>
2014-01-07 13:22:20 +0100

    sendgrid setting config action3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,8 +83,8 @@ FakturaApi::Application.configure do
       :address        => 'smtp.sendgrid.net',
       :port           => '587',
       :authentication => :plain,
-      :user_name      => ENV['faktura-api'],
-      :password       => ENV['qwerty'],
+      :user_name      => ENV['SENDGRID_USERNAME'],
+      :password       => ENV['SENDGRID_PASSWORD'],
       :domain         => 'heroku.com',
       :enable_starttls_auto => true
   }
